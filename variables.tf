41ebb3e6cbacee2d588f66bc31dcc049cb90e7b6
Azeez Razaq Java <52239151+abrotechit@users.noreply.github.com>
2024-10-30 13:39:27 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "location" {
   type        = string
   default     = "East US"
 }
+
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+}
