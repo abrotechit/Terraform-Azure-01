f3474bc13e87ffa26488c064b564fb0661578474
Azeez Razaq Java <52239151+abrotechit@users.noreply.github.com>
2024-11-04 10:26:31 +0100

    Update main.tf

--- iam/main.tf
+++ iam/main.tf
@@ -1,31 +1,41 @@
 terraform{
+required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.
+
+}
 
 provider "azurerm" {
   features {}
 }
 
 provider "azuread" {
-  tenant_id = "<YOUR_TENANT_ID>" # Replace with your actual tenant ID
+  tenant_id = "ea2e0df1-ee30-4c94-9572-0afd18aeb506" # Also called Parent management group in the Azure Portal UI
 }
 
 # Create a Resource Group for Scope of Role Assignment
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "alluserg" {
   name     = "standard-user-resources"
   location = "East US"
 }
 
 # Create an Azure AD User
 resource "azuread_user" "standard_user" {
-  user_principal_name = "standarduser@a2zcybersecgmail.onmicrosoft.com"
+  user_principal_name = "standarduser1@a2zcybersecgmail.onmicrosoft.com"
   display_name        = "Standard User"
   mail_nickname       = "standarduser"
-  password            = "YourSecurePasswordHere" # For production, use a secure method
+  password            = "test123" # For production, use a secure method
 }
 
 # Assign Reader Role to the User on the Resource Group Scope
 resource "azurerm_role_assignment" "reader_role_assignment" {
   principal_id         = azuread_user.standard_user.object_id
   role_definition_name = "Reader" # Assigns Reader role for view-only access
-  scope                = azurerm_resource_group.example.id
+  scope                = azurerm_resource_group.alluserg.id
 }
-}
+
